313e5d0ae902cb06d4f761c9065fc2022ed82e0c
Xasanbek <abdomitovbek@gmail.com>
2024-12-07 15:58:55 +0500

    w3school

---
+++ python/introToPyhton.py
@@ -0,0 +1,7 @@
+# python file name must end with .py
+# to check pyhton is installed on you mac or not run this command on terminal (python --version)
+# you can run your python file on terminal using (python fileName.py)
+
+import sys
+
+print(sys.version)
