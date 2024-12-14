a57cb4a9a91cbc6f8f5599239ad52b1cd7c3be28
Xasanbek <abdomitovbek@gmail.com>
2024-12-14 18:00:37 +0500

    w3school Pyhton lesson 12

---
+++ python/12ConcatenateStrings.py
@@ -0,0 +1,21 @@
+'''
+To concatenate, or combine, two strings you can use the + operator.
+'''
+
+# Merge variable a with variable b into variable c:
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c) #output is HelloWorld
+# To add a space between them, add a " ":
+c = a+" "+b
+print(c) #output is Hello World
+# Or you can change the a or b:
+# a = "Hello " #added space after Hello or b = " World" #added space before World
+# c = a + b
+print(c) #output is Hello World
+# or you can simply Merge them in print no need variable example below
+print(a+b)
+# to add space between them
+print(a+" "+b)
